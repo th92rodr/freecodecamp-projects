1d771b17be6b789de641c39c75510c7ab333ac36
th92rodr <45070690+th92rodr@users.noreply.github.com>
2019-08-22 21:34:28 -0300

    Update Roman Numeral Converter Project

--- Javascript-Algorithms-And-Data-Structures/roman-numeral-converter.js
+++ Javascript-Algorithms-And-Data-Structures/roman-numeral-converter.js
@@ -1,86 +1,102 @@
+/**
+ * Convert the given number into a roman numeral.
+ * 
+ * All roman numerals answers should be provided in upper-case.
+ * 
+ **/
+const DIGIT_1 = 3
+const DIGIT_2 = 2
+const DIGIT_3 = 1
+const DIGIT_4 = 0
+
 function convertToRoman(num) {
     var first_digit = (num / 1) % 10;
     var second_digit = Math.floor((num / 10) % 10);
     var third_digit = Math.floor((num / 100) % 10);
+    var fourth_digit = Math.floor((num / 1000) % 10);
 
-    var list = [];
-    list.push(first_digit);
-    list.push(second_digit);
-    list.push(third_digit);
+    var list = [fourth_digit, third_digit, second_digit, first_digit];
+    var final_result = '';
 
-    // console.log(first_digit);
-    // console.log(second_digit);
-
-    for(var i = 0; i < list.length; i++){
-        console.log(list[i]);
+    for (var i = 0; i < list.length; i++) {
         var number = list[i];
-
-        if(number > 0 && number < 4) {
+        
+        if (number > 0 && number < 4) {
             var res;
-            if(i == 0) {
-                // console.log('I');
-                res = 'I';
-            } else if (i == 1) {
-                // console.log('X');
-                res = 'X';
-            } else if (i == 2) {
-                // console.log('C');
-                res = 'C';
-            }
-            var final_result = '';
-            for(var j = 0; j < number; j++) {
-                // final_result = final_result.concat(res);
+            if (i == DIGIT_1) res = 'I';
+            else if (i == DIGIT_2) res = 'X';
+            else if (i == DIGIT_3) res = 'C';
+            else if (i == DIGIT_4) res = 'M';
+            
+            for (var j = 0; j < number; j++) {
                 final_result = final_result + res;
+                //final_result = final_result.concat(res);
             }
-            console.log(final_result);
         }
+        else if (number == 4) {
+            if (i == DIGIT_1) final_result = final_result + 'IV';
+            else if (i == DIGIT_2) final_result = final_result + 'XL';
+            else if (i == DIGIT_3) final_result = final_result + 'CD';
+        }
+        else if (number > 4 && number < 9) {
+            var aux, res, res1;
 
-        /*
-        switch(number) {
-            case 1:
-                if(i == 0) {
-                    console.log('I');
-                } else if (i == 1) {
-                    console.log('X');
-                } else if (i == 2) {
-                    console.log('C');
-                }
-                break;
-            case 2:
-                console.log('II');
-                break;
-            case 3:
-                if(i == 0) {
-                    console.log('III'); 
-                } else if (i == 1) {
-                    console.log('XXX');
-                } else if (i == 2) {
-                    console.log('CCC');
-                }
-                break;
-            case 4:
-                IV
-                break;
-            case 5:
-                V
-                break;
-            case 6:
-                console.log('VI');
-                break;
-            case 7:
-                VII
-                break;
-            case 8:
-                break;
-            case 9:
-                break;  
+            if (number == 5) aux = 0
+            else if (number == 6) aux = 1
+            else if (number == 7) aux = 2
+            else if (number == 8) aux = 3
+
+            if (i == DIGIT_1) {
+                res = 'V';
+                res1 = 'I';
+            } else if (i == DIGIT_2) {
+                res = 'L';
+                res1 = 'X';
+            } else if (i == DIGIT_3) {
+                res = 'D';
+                res1 = 'C';
+            }
+
+            final_result = final_result + res;
+            for (var j = 0; j < aux; j++) {
+                final_result = final_result + res1;
+            }            
         }
-        */
+        else if (number == 9) {
+            if (i == DIGIT_1) final_result = final_result + 'IX';
+            else if (i == DIGIT_2) final_result = final_result + 'XC';
+            else if (i == DIGIT_3) final_result = final_result + 'CM';
+        }
     }
-
-    
- 
- return num;
+    console.log(final_result);
+    return final_result
 }
-
-convertToRoman(123);
+/**
+ * Test Cases
+ **/
+convertToRoman(2) // should return "II".
+convertToRoman(3) // should return "III".
+convertToRoman(4) // should return "IV".
+convertToRoman(5) // should return "V".
+convertToRoman(9) // should return "IX".
+convertToRoman(12) // should return "XII".
+convertToRoman(16) // should return "XVI".
+convertToRoman(29) // should return "XXIX".
+convertToRoman(44) // should return "XLIV".
+convertToRoman(45) // should return "XLV"
+convertToRoman(68) // should return "LXVIII"
+convertToRoman(83) // should return "LXXXIII"
+convertToRoman(97) // should return "XCVII"
+convertToRoman(99) // should return "XCIX"
+convertToRoman(400) // should return "CD"
+convertToRoman(500) // should return "D"
+convertToRoman(501) // should return "DI"
+convertToRoman(649) // should return "DCXLIX"
+convertToRoman(798) // should return "DCCXCVIII"
+convertToRoman(891) // should return "DCCCXCI"
+convertToRoman(1000) // should return "M"
+convertToRoman(1004) // should return "MIV"
+convertToRoman(1006) // should return "MVI"
+convertToRoman(1023) // should return "MXXIII"
+convertToRoman(2014) // should return "MMXIV"
+convertToRoman(3999) // should return "MMMCMXCIX"
