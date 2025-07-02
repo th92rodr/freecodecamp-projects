999a3b14ef36b9c0a624641e50fb71bbcdb22b0c
th92rodr <45070690+th92rodr@users.noreply.github.com>
2025-07-02 10:48:14 -0300

    Update Roman Numeral Converter Project description

--- javascript-algorithms-and-data-structures/roman-numeral-converter.js
+++ javascript-algorithms-and-data-structures/roman-numeral-converter.js
@@ -1,7 +1,26 @@
 /**
- * Convert the given number into a roman numeral.
+ * Roman Numeral Converter
  *
+ * Convert the given number into a roman numeral.
  * All roman numerals answers should be provided in upper-case.
+ *
+ * Roman numerals are based on seven symbols and can be written using various combinations to represent Arabic numerals.
+ * For example:
+ * | Roman numerals | Arabic numerals |
+ * |----------------|-----------------|
+ * |     M          |     1000        |
+ * |     CM         |      900        |
+ * |     D          |      500        |
+ * |     CD         |      400        |
+ * |     C          |      100        |
+ * |     XC         |       90        |
+ * |     L          |       50        |
+ * |     XL         |       40        |
+ * |     X          |       10        |
+ * |     IX         |        9        |
+ * |     V          |        5        |
+ * |     IV         |        4        |
+ * |     I          |        1        |
  *
  **/
 
@@ -66,7 +85,7 @@ function convertToRoman(num) {
       else if (i == DIGIT_3) finalResult = finalResult + "CM";
     }
   }
-  console.log(finalResult);
+
   return finalResult;
 }
 
