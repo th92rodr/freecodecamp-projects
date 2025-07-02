5fd87c6f41ccc37c907a6bddf5c9d416a3a3de2d
th92rodr <45070690+th92rodr@users.noreply.github.com>
2025-07-02 10:49:09 -0300

    Update Caesars Cipher Project description

--- javascript-algorithms-and-data-structures/caesars-cipher.js
+++ javascript-algorithms-and-data-structures/caesars-cipher.js
@@ -1,9 +1,12 @@
 /**
+ * Caesars Cipher
+ *
  * One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
  *
  * A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
  *
  * Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+ *
  * All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
  *
  **/
@@ -12,18 +15,14 @@ function rot13(str) {
   const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
   str = str.toLowerCase();
-  //console.log(str);
 
   let str2 = "";
   for (let i = 0; i < str.length; i++) {
-    //console.log(str[i]);
-
     // pass on any non-alphabetic character
     if (
       str[i] == " " ||
       str[i].match(/^[-&\/\\#,+()$~%.'":*\!\?\<>_{}0-9]+$/i)
     ) {
-      //console.log(str[i]);
       str2 = str2 + str[i];
     } else {
       const normalIndex = alphabet.indexOf(str[i]);
@@ -36,12 +35,10 @@ function rot13(str) {
         index = normalIndex + 13;
       }
 
-      //console.log(alphabet[index]);
       str2 = str2 + alphabet[index];
     }
   }
 
-  console.log(str2);
   str = str2.toUpperCase();
   return str;
 }
